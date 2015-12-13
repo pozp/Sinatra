3a816e910147d8794018b7458f07f2352e11bf5c
pozp <pawel.podziej@o2.pl>
2015-12-13 13:34:21 +0000

    add files

--- myapp.rb
+++ myapp.rb
@@ -1,26 +1,40 @@
 require 'sinatra'
+require 'data_mapper'
+require 'dm-sqlite-adapter'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/myapp.db")
+
+class Note
+    include DataMapper::Resource
+    property :id, Serial
+    property :content, Text, :required => true
+    property :complete, Boolean, :required => true, :default => false
+    property :created_at, DateTime
+    property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
 
 get '/' do
-    @title = "List full of shit"  #zmienna instancji, czyli nalezaca do danego obiektu tu pełniaca role templatki
-    erb :index #symbol, czyli ":foo" mający jedna niezmienna wartosc, w tym przypadku przypisany do do indexu, bo to kuzwa index nigggga
+    @title = "MyFirst App :)"  #zmienna instancji, czyli nalezaca do danego obiektu tu pełniaca role templatki
+    @notes = Note.all :order => :id.desc
+    erb :index  #symbol, czyli ":foo" mający jedna niezmienna wartosc, w tym przypadku przypisany do indexu, bo to kuzwa index nigggga
 end
 
-post '/vote' do                              #kolejna strona
-   @title = "Another site mothafuka"
-   @vote = params['vote']
-   erb :vote
+post '/' do
+    n = Note.new
+    n.content = params[:content]
+    n.created_at = Time.now
+    n.updated_at = Time.now
+    n.save
+    redirect '/'
 end
 
-get '/results' do 
-    @votes ={}
-    erb :results
+
+get '/about' do   #kolejna strona
+    @title = "About"
+    erb :about
 end
     
 
-Choices = {
-    "LEN" => "Learn",
-    "COK" => "Cook",
-    "SMI" => "Smile",
-}
-   
 
