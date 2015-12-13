1890b4cf1cbb77d7b59919ba2cf9d9e651fa3075
pozp <pawel.podziej@o2.pl>
2015-12-13 18:23:32 +0000

    upgrade

--- myapp.rb
+++ myapp.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'data_mapper'
 require 'sqlite3'
-require 'dm-sqlite-adapter'
+require 'dm-sqlite-adapter/adapter'
 
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/myapp.db")
